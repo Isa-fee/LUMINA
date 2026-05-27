c6d734c14df25a723dedbd8ebe194c3186bd1478
Isa-fee <tiagosoaresjp1995@gmail.com>
2026-05-27 11:38:05 -0300

    criação das primeiras páginas html, que já estão abrindo nas rotas, e criação do requirements

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from contextlib import asynccontextmanager
 from sqlmodel import SQLModel, Session
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from database import engine, get_session
 
@@ -30,6 +32,26 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
+# =========================
+# PÁGINA INICIAL
+# =========================
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"request": request}
+    )
 
 # =========================
 # USUARIOS
@@ -69,7 +91,24 @@ def deletar_usuario(
 
 # =========================
 # LIVROS
-# =========================
+# ========================
+
+@app.get("/pagina-livros")
+def pagina_livros(
+    request: Request,
+    session: Session = Depends(get_session)
+):
+
+    livros = crud.listar_livros(session)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="livros.html",
+        context={
+            "request": request,
+            "livros": livros
+        }
+    )
 
 @app.post("/livros")
 def criar_livro(
@@ -77,13 +116,6 @@ def criar_livro(
     session: Session = Depends(get_session)
 ):
     return crud.criar_livro(session, livro)
-
-
-@app.get("/livros")
-def listar_livros(
-    session: Session = Depends(get_session)
-):
-    return crud.listar_livros(session)
 
 
 @app.put("/livros/{livro_id}")
@@ -104,12 +136,53 @@ def deletar_livro(
 
 
 # =========================
-# EMPRESTIMOS
+# LOGIN
 # =========================
+
+@app.get("/login")
+def pagina_login(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+        context={"request": request}
+    )
+
+# =========================
+# CADASTRO
+# =========================
+
+@app.get("/cadastro")
+def pagina_cadastro(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="cadastro.html",
+        context={"request": request}
+    )
+
+# =========================
+# CADASTRO DE LIVROS
+# =========================
+
+@app.get("/cadastro-livro")
+def pagina_cadastro_livro(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="cadastroLivro.html",
+        context={"request": request}
+    )
+
 
 # =========================
 # EMPRESTIMOS
 # =========================
+
+@app.get("/pagina-emprestimos")
+def pagina_emprestimos(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="emprestimos.html",
+        context={"request": request}
+    )
 
 @app.post("/emprestimos")
 def criar_emprestimo(
